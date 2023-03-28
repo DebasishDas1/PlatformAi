4f927755f576328d4b492292054636ce00945d24
Debasisg Das <iamdebasishno1@gmail.com>
2023-03-28 06:31:04 +0530

    1st_commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import styles from './app.module.css';
+
+const App = () => {
+    return (
+        <div className={styles.app} >
+            Platform Ai
+        </div>
+    );
+};
+
+export default App;
